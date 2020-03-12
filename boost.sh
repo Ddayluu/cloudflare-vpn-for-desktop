b39e8d0442cbb98e1278eef1458d6b89ff9ac3df
DatLuu <37364006+Ddayluu@users.noreply.github.com>
2020-03-12 12:16:02 +0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -4,7 +4,7 @@
 # Scripted by Tuyen Tran
 # http://fb.com/gr219
 # -------------------
-referal=$1
+referal='ae929c3c-1ffe-4e4d-8c3d-fa3c3c22715b'
 interval=$2
 
 docker run -v `pwd`/getdata.sh:/getdata.sh --rm -i alpine sh -c \
